045e3ae3e4676b196254c01afda93412f50c6747
Peter Fänge <pellefange@gmail.com>
2025-03-27 12:11:57 +0100

    Added new route and middleware

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ app.get('/', (req, res) => {
     res.send("Welcome to our API")
 });
 
+// Assign a new route with /users which contains users' information
+app.get('/users', (req, res) => {
+    const users = [
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Jane Smith" },
+    ];
+    res.json(users);
+})
+
+// Middleware to parse the JSON data
+app.use(express.json());
+
+app.post ("/users", (req, res) => {
+    const newUser = req.body;
+    console.log(newUser); // Log the data
+    res.json({ message: "Successfully added user.", user: newUser })
+})
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running http://localhost:${PORT}`)
